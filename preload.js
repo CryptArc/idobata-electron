8e52002b7fe8797825cd98f39d0900b2b6c3f0bb
aikyo <aikyo02@gmail.com>
2015-07-14 11:38:16 +0900

    Enable to notify only mentioned.

--- preload.js
+++ preload.js
@@ -1,22 +1,31 @@
 (function(){
-  var onMessageCreated = function(user, store) {
+  var onMessageCreated = function(session, store) {
     return function(data) {
-      store.find('message', data.message.id).then(function(message) {
+      var mode = 'all';
+      var notify = false;
+      if (mode == "all") {
+        notify = true;
+      } else if (mode == "mention") {
+        if (data.message.mentions.indexOf(parseInt(session.user.id)) >= 0) {
+          notify = true;
+        }
+      }
+      if (notify) {
         var title = data.message.senderName;
         new Notification(title, {
           body: data.message.bodyPlain,
           icon: data.message.senderIconUrl
         });
-      });
+      };
     }
   };
 
   window.addEventListener('ready.idobata', function(e) {
     var container = e.detail.container;
-    var pusher = container.lookup('pusher:main');
-    var user   = container.lookup('service:session').get('user.id');
-    var store  = container.lookup('store:main');
+    var pusher  = container.lookup('pusher:main');
+    var session = container.lookup('service:session');
+    var store   = container.lookup('store:main');
 
-    pusher.bind('message:created', onMessageCreated(user, store));
+    pusher.bind('message:created', onMessageCreated(session, store));
   });
 })();
